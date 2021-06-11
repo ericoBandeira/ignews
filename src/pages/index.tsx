fb90dc565c694b21ac38499711f43afc80fca293
ericoBandeira <ericobandeira@hotmail.com>
2021-06-11 10:50:02 -0300

    add base project

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | Ig.news</title>
+      </Head>
+      <h1>
+      
+        Jello <span>Pello</span>
+      </h1>
+    </>
+  );
+}
